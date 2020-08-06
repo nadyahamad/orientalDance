1f4affb9f63fdd19f71fab02d49fd690cdf8cfe6
Nahamad <nahamad83@gmail.com>
2020-08-06 03:24:00 +0100

    usability improvements

--- controllers/auth.js
+++ controllers/auth.js
@@ -99,7 +99,7 @@ exports.postLogin = (req, res, next) => {
           req.session.user = user;
           return req.session.save(err => {
             console.log(err);
-            res.redirect('/classes');
+            res.redirect('/landing');
           });
         }
         return res.status(422).render('auth/login', {
